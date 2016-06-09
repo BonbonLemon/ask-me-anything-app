a6f168ca462275485b6c5687165808bad5071f33
Stephen Chen <chen.stephen.r@gmail.com>
2016-06-09 12:47:02 -0700

    initial commit

---
+++ askmeanything/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+import home.views
+
+urlpatterns = [
+    url(r'^$', home.views.home),
+    url(r'^admin/', admin.site.urls),
+]
